db4d06514d34380954fc07df656e2c77f670bc77
camenduru <54370274+camenduru@users.noreply.github.com>
2023-09-08 20:05:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,21 +11,11 @@ from model import ADAPTER_NAMES, Model, download_all_adapters
 
 DESCRIPTION = "# T2I-Adapter-SDXL"
 
-if not torch.cuda.is_available():
-    DESCRIPTION += "\n<p>Running on CPU 🥶 This demo does not work on CPU.</p>"
-
-
 download_all_adapters()
 model = Model(ADAPTER_NAMES[0])
 
-
 with gr.Blocks(css="style.css") as demo:
     gr.Markdown(DESCRIPTION)
-    gr.DuplicateButton(
-        value="Duplicate Space for private use",
-        elem_id="duplicate-button",
-        visible=os.getenv("SHOW_DUPLICATE_BUTTON") == "1",
-    )
     with gr.Tabs():
         with gr.Tab(label="Base"):
             create_demo_base(model)
